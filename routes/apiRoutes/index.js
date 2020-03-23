e71d5fee099c41e2b6d1b28d4ab1f9d1de6359b5
suneethaburla <suneetha@gmail.com>
2020-03-22 19:07:51 -0700

    updated tableshtml

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -2,6 +2,8 @@ const router = require('express'). Router();
 //we have one for /reserve and one for /tables
 //this folder had /api prepended to everything
 
+const db = require('./../../models')
+
 const tablesApi = require('./tablesApi');
 const waitlistApi = require('./waitlistApi');
 
@@ -9,6 +11,15 @@ const waitlistApi = require('./waitlistApi');
 router.use('/tables', tablesApi);
 
 router.use('/waitlist', waitlistApi);
+
+//when you clear the tables, it will delete everything in the array
+
+router.route ('/clear')
+.get((req, res) => {
+db.Reserve.length = 0;
+db.Waitlist.length = 0;
+res.send("cleared")
+})
 
 
 module.exports = router;
